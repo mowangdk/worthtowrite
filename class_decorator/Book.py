5d191882fe2a1f4d7a14538b5a969c0423262944
mowangdk <geyingqi0701@gmail.com>
2016-08-27 15:20:16 +0800

    class_decorator

---
+++ class_decorator/Book.py
@@ -0,0 +1,56 @@
+import numbers
+
+
+@ensure('title', is_non_empty_str)
+@ensure('isbn', is_valid_isbn)
+@ensure('price', is_in_range(1, 10000))
+@ensure('title', is_in_range(0, 1000000))
+class Book(object):
+    def __init__(self, title, isbn, price, quantity):
+        self.title = title
+        self.isbn = isbn
+        self.price = price
+        self.quantity = quantity
+
+    @property
+    def value(self):
+        return self.price * self.quantity
+
+
+# 这个验证器是用来确保book和title的属性不是空字符串
+def is_non_empty_str(name, value):
+    if not isinstance(value, str):
+        raise ValueError("{} must be type of str".format(name))
+    if not bool(value):
+        raise ValueError("{} may not be empty".format(name))
+
+
+# 这个函数是一个工厂函数,每次调用的时候就会创建新的验证器函数
+def is_in_range(minimum=None, maxinum=None):
+    assert minimum is not None or maxinum is not None
+
+    def is_in_range(name, value):
+        if not isinstance(value, numbers.Number):
+            raise ValueError("{} must be a number".format(name))
+        if minimum is not None and value < minimum:
+            raise ValueError("{},{} is too small".format(name, value))
+        if maxinum is not None and value > maxinum:
+            raise ValueError("{},{} is too big".format(name, value))
+    return is_in_range
+
+
+# ensure函数会以给定的属性名,验证器函数以及可选的docstring为参数来创造类修饰器
+def ensure(name, validate, doc=None):
+    def decorator(Class):
+
+        privateName = '__'+name
+
+        def getter(self):
+            return getattr(self, privateName)
+
+        def setter(self, value):
+            validate(name, value)
+            return setattr(self, privateName, value)
+        setattr(Class, name, property(getter, setter, doc=doc))
+        return Class
+    return decorator
---
+++ class_decorator/Book_improve.py
@@ -0,0 +1,61 @@
+
+@do_ensure
+class Book:
+    title = Ensure(is_non_empty_str)
+    price = Ensure(is_in_range(1, 10000))
+    quantity = Ensure(is_in_range(0, 1000000))
+
+    def __init__(self, title, price, quantity):
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+
+    @property
+    def value(self):
+        return self.price * self.quantity
+
+
+# 这个验证器是用来确保book和title的属性不是空字符串
+def is_non_empty_str(name, value):
+    if not isinstance(value, str):
+        raise ValueError("{} must be type of str".format(name))
+    if not bool(value):
+        raise ValueError("{} may not be empty".format(name))
+
+
+# 这个函数是一个工厂函数,每次调用的时候就会创建新的验证器函数
+def is_in_range(minimum=None, maxinum=None):
+    assert minimum is not None or maxinum is not None
+
+    def is_in_range(name, value):
+        if not isinstance(value, numbers.Number):
+            raise ValueError("{} must be a number".format(name))
+        if minimum is not None and value < minimum:
+            raise ValueError("{},{} is too small".format(name, value))
+        if maxinum is not None and value > maxinum:
+            raise ValueError("{},{} is too big".format(name, value))
+    return is_in_range
+
+
+class Ensure(object):
+    def __init__(self, validate, doc=None):
+        self.validate = validate
+        self.doc = doc
+
+
+def do_ensure(Class):
+    def make_property(name, attribute):
+        private_name = "__"+name
+        def getter(self):
+            return getattr(self, private_name)
+        def setter(self, value):
+            attribute.validate(name, value)
+            return setattr(self, private_name, value)
+        return property(getter, setter, doc=attribute.doc)
+    for name, attribute in Class.__dict__.items():
+        if isinstance(attribute, Ensure):
+            setattr(Class, name, make_property(name, attribute))
+    return Class
+
+
+
